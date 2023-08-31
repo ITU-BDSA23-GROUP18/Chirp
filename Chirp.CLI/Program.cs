21fa96abd511c2bfdc940b265cc5e187e2eaa357
Benjamin Altenburg <benjaminvba@gmail.com>
2023-08-31 16:00:24 +0200

    Create exception and fix if-statement

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,7 +1,12 @@
 ﻿if (args[0] == "cheep")
 {
-    if (args.Length >= 2 && args[1] == "") return;
+    if (args.Length < 2 || args[1] == "")
+    {
+        throw new Exception("What is your message?");
+        return;
+    }
     
+
     var writer = File.AppendText("./chirp_cli_db.csv");
     var message = args[1];
     var user = Environment.UserName;
