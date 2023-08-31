7950210a7ea6cf3e89300d478de1d99f81cbc54f
JackITU <jkrs@itu.dk>
2023-08-31 15:37:43 +0200

    Cannot save empty cheeps Co-authored-by: David <dafe@itu.dk> Co-authored-by: Benjamin <beal@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿if (args[0] == "cheep")
 {
+    if (args.Length >= 2 && args[1] == "") return;
+    
     var writer = File.AppendText("./chirp_cli_db.csv");
     var message = args[1];
     var user = Environment.UserName;
