947379a71b4a92e54ab32254749c27f6178481f5
JackITU <jkrs@itu.dk>
2023-08-31 15:38:30 +0200

    Cheeps dont write spaces to csv

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -6,6 +6,6 @@
     var message = args[1];
     var user = Environment.UserName;
     var time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-    writer.WriteLine($"{user}, \"{message}\", {time}");
+    writer.WriteLine($"{user},\"{message}\",{time}");
     writer.Close();
 }
