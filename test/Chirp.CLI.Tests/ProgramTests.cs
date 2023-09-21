3bd098c6584486d39c74611b080b5be510a79069
JackITU <jkrs@itu.dk>
2023-09-21 15:43:57 +0200

    Test runs a new process Co-authored-by: Helge <ropf@itu.dk>

--- test/Chirp.CLI.Tests/ProgramTests.cs
+++ test/Chirp.CLI.Tests/ProgramTests.cs
@@ -1,10 +1,35 @@
+using System.Diagnostics;
+
 namespace Chirp.CLI.Tests;
 
 public class ProgramTests
 {
     [Fact]
-    public void Test1()
+    public void TestReadCheep()
     {
+        // Arrange
+        //ArrangeTestDatabase();
+        // Act
+        string output = "";
+        using (var process = new Process())
+        {
+            process.StartInfo.FileName = "../../../../../src/Chirp.CSVDBService/bin/Debug/net7.0/Chirp.CSVDBService.exe";
+            process.StartInfo.Arguments = "";
+            process.StartInfo.UseShellExecute = false;
+            // process.StartInfo.WorkingDirectory = "";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+            // Synchronously read the standard output of the spawned process.
+            // StreamReader reader = process.StandardOutput;
+            // output = reader.ReadToEnd();
+            // process.WaitForExit();
 
+            var line = process.StandardOutput.ReadLine();
+            throw new Exception("YEAH BOI" + line);
+        }
+        string fstCheep = output.Split("\n")[0];
+        // Assert
+        Assert.StartsWith("ropf", fstCheep);
+        Assert.EndsWith("Hello, World!", fstCheep);
     }
 }
