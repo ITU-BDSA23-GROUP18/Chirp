6a8d94719a077bed0b593aa7ae798574924787d3
JackITU <jkrs@itu.dk>
2023-10-05 15:41:24 +0200

    Made CheepRepository inferriting from IRepository<Cheep>

---
+++ src/Repositories/CheepRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace Repositories;
+
+public class CheepRepository : IRepository<Cheep, Author>
+{
+    private readonly DBFacade _cheepDB;
+
+    public CheepRepository(DBFacade cheepDB)
+    {
+        _cheepDB = cheepDB;
+    }
+
+    public IEnumerable<Cheep> Get() =>
+        from c in _cheepDB.GetCheeps(0, 32)
+        select new Cheep(new Author(c.Author, ""), c.Message, DateTime.Parse(c.Timestamp));
+
+    public IEnumerable<Cheep> GetFrom(Author attribute) =>
+        from c in _cheepDB.GetCheeps(0, 32)
+        where c.Author == attribute.Name
+        select new Cheep(new Author(c.Author, ""), c.Message, DateTime.Parse(c.Timestamp));
+}
