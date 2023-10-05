57d2da759c3ee982a866064d47b781fb99958923
Mads Valentin Jensen <mavjdk@gmail.com>
2023-10-05 19:25:51 +0200

    added DTO class with the MainCheep

---
+++ src/Repositories/DTO.cs
@@ -0,0 +1,9 @@
+namespace Repositories.DTO
+{
+    public class MainCheep
+    {
+       public required string Author {get; set;}
+       public required string Message {get; set;}
+       public required int time {get; set;}
+    }
+}
