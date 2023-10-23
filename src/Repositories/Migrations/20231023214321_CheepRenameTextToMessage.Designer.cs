98de20da37b9b1c9a8e1c066b3c78d71ef8257b7
David Feldner <dafe@itu.dk>
2023-10-23 23:47:16 +0200

    Add missing migration for cheep message rename

--- src/Repositories/Migrations/20231023214321_CheepRenameTextToMessage.Designer.cs
+++ src/Repositories/Migrations/20231023214321_CheepRenameTextToMessage.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Repositories;
 
@@ -10,9 +11,11 @@ using Repositories;
 namespace Repositories.Migrations
 {
     [DbContext(typeof(ChirpContext))]
-    partial class CheepContextModelSnapshot : ModelSnapshot
+    [Migration("20231023214321_CheepRenameTextToMessage")]
+    partial class CheepRenameTextToMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.12");
