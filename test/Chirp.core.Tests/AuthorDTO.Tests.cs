024950e4ea485938bf93585ad143b3205534fd74
Mads Valentin Jensen <mvje@itu.dk>
2023-11-14 14:59:39 +0100

    added AuthorDTO.Tests and CheepDTO.Tests. Also added refrence to Chirp.core

---
+++ test/Chirp.core.Tests/AuthorDTO.Tests.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+
+namespace Chirp.core.Tests;
+
+public class authorDTOTests
+{
+    [Theory]
+    [InlineData("test1", null)]
+    [InlineData("", "test2")]
+    public void CreateAuthorDTOTest(string name, string email)
+    {
+        
+        var authorDTO= new AuthorDTO(name, email);
+
+        Assert.Equal(authorDTO.Name, authorDTO.Name);
+        Assert.Equal(authorDTO.Email, authorDTO.Email);
+    }
+
+    [Fact]
+    public void CreatenAoutherWithNullNameTest()
+    {
+        var authorDTO= new AuthorDTO(null, null);
+        Debug.WriteLine(authorDTO.Name);
+
+    }
+}
---
+++ test/Chirp.core.Tests/CheepDTO.Tests.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+
+namespace Chirp.core.Tests;
+
+public class CheepDTOTests
+{
+    [Theory]
+    [InlineData("test1", null, null)]
+    [InlineData("", "test2",null)]
+    public void CreateCheepDTOTest(string author, string message, string timestamp)
+    {
+        
+        var cheepDTO= new CheepDTO(author, message, timestamp);
+
+        Assert.Equal(cheepDTO.Author, cheepDTO.Author);
+        Assert.Equal(cheepDTO.Message, cheepDTO.Timestamp);
+        Assert.Equal(cheepDTO.Timestamp, cheepDTO.Timestamp);
+    }
+
+    [Fact]
+    public void CreatenAoutherWithNullNameTest()
+    {
+        var authorDTO= new AuthorDTO(null, null);
+        Debug.WriteLine(authorDTO.Name);
+
+    }
+}
