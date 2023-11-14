18a91748858370a52573fee5bf6ffe617df23a30
Mads Valentin Jensen <mvje@itu.dk>
2023-11-14 15:17:42 +0100

    changed CheepDTO such that we may add constraints in the constructor

--- src/Chirp.core/DTOs/CheepDTO.cs
+++ src/Chirp.core/DTOs/CheepDTO.cs
@@ -1,3 +1,12 @@
 namespace Chirp.core;
 
-public record CheepDTO(string Author, string Message, string Timestamp);
+public record CheepDTO(string Author, string Message, string Timestamp){
+    //the code below allows us to create a CheepDTO and handle null values
+    public CheepDTO() : this(default!, default!, default!) {
+        if(Author is null) throw new ArgumentNullException("Author cannot be null");
+        if(Message is null) throw new ArgumentNullException("Message cannot be null");
+        if(Timestamp is null) throw new ArgumentNullException("Timestamp cannot be null");
+    }
+    
+    public int maxLenght = 160;
+}
