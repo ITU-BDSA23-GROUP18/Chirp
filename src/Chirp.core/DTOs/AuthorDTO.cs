977b93a1fb62df9273c24b5c09c88a1e0ad88e27
Mads Valentin Jensen <mvje@itu.dk>
2023-11-14 15:48:37 +0100

    added changes to the DTO's such that we might check for null values and max value. This is wary unlikly to happen because the dto are only create in the repositoryes

--- src/Chirp.core/DTOs/AuthorDTO.cs
+++ src/Chirp.core/DTOs/AuthorDTO.cs
@@ -1,3 +1,11 @@
 ﻿namespace Chirp.core;
 
-public record AuthorDTO(string Name, string? Email);
+public record AuthorDTO(string Name, string? Email){
+    //the code below allows us to create a AuthorDTO and handle null values
+    //the dtos are only created on the server side, so it is ware unlikely that we will get null values unless we are testing
+    public AuthorDTO() : this(default!, default!) {
+        if(Name is null) throw new ArgumentNullException("Name cannot be null");
+        //if we in the future decide to make email mandatory, we can uncomment the line below
+        //if(Email is null) throw new ArgumentNullException("Email cannot be null");
+    }
+}
