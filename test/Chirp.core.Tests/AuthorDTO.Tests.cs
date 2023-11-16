b10ed7fe39bc321d1d4290c609e35dc09cf82a54
Mads Valentin Jensen <mavjdk@gmail.com>
2023-11-16 12:45:58 +0100

    added a test for null values in DTO tests

--- test/Chirp.core.Tests/AuthorDTO.Tests.cs
+++ test/Chirp.core.Tests/AuthorDTO.Tests.cs
@@ -6,8 +6,7 @@ public class authorDTOTests
 {
     [Theory]
     [InlineData("test1", null)]
-    [InlineData("", "test2")]
-    [InlineData(null,null)]
+    [InlineData("test2", "test3")]
     public void CreateAuthorDTOTest(string name, string email)
     {
         var authorDTO= new AuthorDTO(name, email);
@@ -15,5 +14,15 @@ public class authorDTOTests
         Assert.Equal(authorDTO.Name, authorDTO.Name);
         Assert.Equal(authorDTO.Email, authorDTO.Email);
     }
-
+    
+    [Theory]
+    [InlineData(null,"m")]
+    [InlineData("","m")]
+    public void AuthorDTONullValueTest(string name, string email){
+        var exceptionType = typeof(ArgumentNullException);
+        
+        Assert.Throws(exceptionType, () => {
+            var CheepDTO = new AuthorDTO(name, email);
+        });
+    }
 }
--- test/Chirp.core.Tests/CheepDTO.Tests.cs
+++ test/Chirp.core.Tests/CheepDTO.Tests.cs
@@ -5,17 +5,30 @@ namespace Chirp.core.Tests;
 public class CheepDTOTests
 {
     [Theory]
-    [InlineData(null,null,null)]
-    [InlineData("test1", null, null)]
-    [InlineData("", "test2",null)]
     [InlineData("test1","test2","test3")]
+    [InlineData("test2","test3","test4")]
+    [InlineData("test3","test4","test5")]
     public void CreateCheepDTOTest(string author, string message, string timestamp)
     {
-        
         var cheepDTO= new CheepDTO(author, message, timestamp);
 
         Assert.Equal(cheepDTO.Author, cheepDTO.Author);
-        Assert.Equal(cheepDTO.Message, cheepDTO.Timestamp);
+        Assert.Equal(cheepDTO.Message, cheepDTO.Message);
         Assert.Equal(cheepDTO.Timestamp, cheepDTO.Timestamp);
+    }
+    [Theory]
+    [InlineData(null,"test2","test3")]
+    [InlineData("test2",null,"test4")]
+    [InlineData("test3","test4",null)]
+    [InlineData("","test2","test3")]
+    [InlineData("test2","","test4")]
+    [InlineData("test3","test4","")]
+    public void NullValuesInCheepDTOTest(string author, string message, string timestamp){
+        
+        var exceptionType = typeof(ArgumentNullException);
+        
+        Assert.Throws(exceptionType, () => {
+            var CheepDTO = new CheepDTO(author, message, timestamp);
+        });
     }
 }
