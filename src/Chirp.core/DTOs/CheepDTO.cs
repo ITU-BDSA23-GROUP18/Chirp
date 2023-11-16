2fde74be85054c08d28887ffd069ff7460fe5d39
Mads Valentin Jensen <mavjdk@gmail.com>
2023-11-16 11:51:20 +0100

    expanded CheepDTO to check

--- src/Chirp.core/DTOs/CheepDTO.cs
+++ src/Chirp.core/DTOs/CheepDTO.cs
@@ -1,3 +1,20 @@
+using System.Buffers;
+
 namespace Chirp.core;
 
-public record CheepDTO(string Author, string Message, string Timestamp);
+public record CheepDTO{
+    string Author{get;set;}
+    string Message{get;set;} 
+    string Timestamp{get;set;}
+
+    public CheepDTO(string author, string message, string timestamp)
+    {
+        if(author == null||author.Equals("")) throw new ArgumentNullException("author is null or empty");
+        if(message == null||message.Equals("")) throw new ArgumentNullException("message is null or empty");
+        if(timestamp == null||timestamp.Equals("")) throw new ArgumentNullException("timestamp is null or empty");
+        Author = author;
+        Message = message;
+        Timestamp = timestamp;
+    }
+
+}
