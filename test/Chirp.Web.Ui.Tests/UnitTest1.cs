21b67ed0e732cc4fd37652cc1c11fe04967f5cda
Mads Valentin Jensen <mavjdk@gmail.com>
2023-11-20 17:55:04 +0100

    added test project and fixed some errors

---
+++ test/Chirp.Web.Ui.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+    namespace Chirp.Web.Ui.Tests;
+
+    using System.Threading.Tasks;
+    using Microsoft.Playwright;
+    using Microsoft.Playwright.NUnit;
+    using Playwright.App.Tests.Infrastructure;
+    using Xunit;
+
+    public class UiTest : PageTest, IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly string _serverAddress;
+
+        public UiTest(CustomWebApplicationFactory fixture)
+        {
+            _serverAddress = fixture.ServerAddress;
+        }
+
+        [Fact]
+        public async Task Navigate_to_counter_ensure_current_counter_increases_on_click()
+        {
+
+            //Arrange
+            using var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new Microsoft.Playwright.BrowserTypeLaunchOptions{
+                Headless = false,
+                SlowMo = 600,
+            });
+            var contextOptions = new BrowserNewContextOptions()
+            {
+                IgnoreHTTPSErrors = true // This will ignore HTTPS errors
+            };
+            await using var context = await browser.NewContextAsync(contextOptions);
+
+            var page = await context.NewPageAsync();
+
+            
+
+            await page.GotoAsync(_serverAddress);
+
+            //Act
+        }
+    }
