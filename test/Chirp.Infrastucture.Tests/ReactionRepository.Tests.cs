3e231a750982dd9da5021a186142994fae208a31
Mads Valentin Jensen <mavjdk@gmail.com>
2023-12-11 16:34:55 +0100

    create a test to create a recation

--- test/Chirp.Infrastucture.Tests/ReactionRepository.Tests.cs
+++ test/Chirp.Infrastucture.Tests/ReactionRepository.Tests.cs
@@ -1,18 +1,39 @@
-﻿namespace Chirp.Infrastructure.Tests;
+﻿using Microsoft.Data.Sqlite;
+
+namespace Chirp.Infrastructure.Tests;
 
 public class ReactionRepository_Tests
 {
-/*
-    [Fact]
-    public void GetAllReactionTypes_returnsAllReactionTypeStrings()
+    private readonly ChirpContext _context;
+    private readonly ReactionRepository _repository;
+
+    private readonly CheepRepository _cheep_repository;
+
+    public ReactionRepository_Tests()
     {
-        IReactionRepository _repository = null;
-        var reactionStrings = _repository.GetAllReactionTypes();
-        
-        var reactionTypes = reactionStrings.Select(r => Enum.Parse<ReactionType>(r));
-        
-        var allReactionTypes = Enum.GetValues<ReactionType>();
-        Assert.All(reactionTypes, r => 
-            Assert.Contains(r, allReactionTypes));
-    }*/
+        var connection = new SqliteConnection("Filename=:memory:");
+        connection.Open();
+        var builder = new DbContextOptionsBuilder<ChirpContext>().UseSqlite(connection);
+        _context = new ChirpContext(builder.Options);
+        _repository = new ReactionRepository(_context);
+        _cheep_repository = new CheepRepository(_context);
+    }
+    [Theory]
+    [InlineData("reactionString")]
+    public async Task TestCreateReaction(string reactionString)
+    {
+        var cheep = _context.Cheeps.FirstOrDefault();
+        _repository.CreateReaction( cheep.CheepId.ToString() , cheep.Author.Name.ToString(), "Good");
+        var reaction = _context.Reactions.FirstOrDefault();
+        Assert.NotNull(reaction);
+        Assert.Equal(cheep.CheepId.ToString() , reaction.CheepId.ToString());
+        Assert.Equal(cheep.CheepId.ToString(), reaction.AuthorName);
+        Assert.Equal(ReactionType.Good, reaction.ReactionType);
+    }
+    [Fact]
+    public async Task TestGetReaction()
+    {
+
+    }
+
 }
