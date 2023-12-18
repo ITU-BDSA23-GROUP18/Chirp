d57001df2aff4c32d600882014d6155c1f1e1ffa
davidfeldner <dafe@itu.dk>
2023-12-18 14:37:11 +0100

    Formatting and add local password file for azure back

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -16,23 +16,26 @@ public class Program
         builder.Services.AddRazorPages().AddMicrosoftIdentityUI();
 
         /*var dbPath = Path.Combine(Path.GetTempPath(), "Chirp.db");
-        builder.Services.AddDbContext<ChirpContext>(options => options.UseSqlite($"Data Source={dbPath}"));
-*/
+        builder.Services.AddDbContext<ChirpContext>(options => options.UseSqlite($"Data Source={dbPath}"));*/
+
         // Try to get remote connection string
-         string? connectionString = builder.Configuration.GetConnectionString("AzureSQLDBConnectionstring");
-         if (connectionString == null) throw new Exception("Connection string not found");
-         if (!connectionString.Contains("Password")) {
-             string? pass = builder.Configuration["Chirp:azuredbkey"];
-             if (pass == null) {
-                 Console.WriteLine("Local sql password not set and was not overriden by remote!");
-             } else {
-                 Console.WriteLine(pass);
-                 connectionString += $"Password={pass};";
-             }
-         }
-  
-         builder.Services.AddDbContext<ChirpContext>(options => options.UseSqlServer(connectionString));
- 
+        string? connectionString = builder.Configuration.GetConnectionString("AzureSQLDBConnectionstring");
+        if (connectionString == null) throw new Exception("Connection string not found");
+        if (!connectionString.Contains("Password"))
+        {
+            string? pass = builder.Configuration["Chirp:azuredbkey"];
+            if (pass == null)
+            {
+                Console.WriteLine("Local sql password not set and was not overriden by remote!");
+            }
+            else
+            {
+                connectionString += $"Password={pass};";
+            }
+        }
+
+        builder.Services.AddDbContext<ChirpContext>(options => options.UseSqlServer(connectionString));
+
         builder.Services.AddScoped<ICheepRepository, CheepRepository>();
         builder.Services.AddScoped<IAuthorRepository, AuthorRepository>();
         builder.Services.AddScoped<IReactionRepository, ReactionRepository>();
@@ -42,15 +45,19 @@ public class Program
         // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
-            /*if (!connectionString.Contains("Password")) {
+            if (!connectionString.Contains("Password"))
+            {
                 string passwordFile = "connectionString.txt";
-                if (!File.Exists(passwordFile)) {
+                if (File.Exists(passwordFile))
+                {
+                    using var sr = new StreamReader(passwordFile);
+                    connectionString += $"Password={sr.ReadToEnd()};";
+                }
+                else
+                {
                     Console.WriteLine("---- No password for sql server, create a file called connectionString.txt with the password in it. ----");
                 }
-                using var sr = new StreamReader(passwordFile);
-                // Read the stream as a string, and write the string to the console.
-                connectionString += $"Password={sr.ReadToEnd()};";
-            } */
+            }
 
             app.UseExceptionHandler("/Error");
 
